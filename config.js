2ac15390fed3e9e6973bf3a85fce0f71894dde1a
Helixa -= L33T =- <59974692+HelixaL33T@users.noreply.github.com>
2020-01-18 18:43:31 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	yourID: "Type_Your_Discord_Id_Here",
+	setupCMD: "!createrolemessage",
+	deleteSetupCMD: false,
+	initialMessage: `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`,
+	embedMessage: `
+	React to the emoji that matches the role you wish to receive.
+	
+	If you would like to remove the role, simply remove your reaction!
+	`,
+	embedFooter: "[ L33T Development ] @ Reactions",
+	roles: ["Hacker", "Artist", "Public Relations", "Intern"],
+
+	reactions: ["💻", "🖌", "😃", "🆕"],
+	embed: true,
+	embedColor: "#dd9323",
+	embedThumbnail: false,
+	embedThumbnailLink: "",
+	botToken: ""
+};
